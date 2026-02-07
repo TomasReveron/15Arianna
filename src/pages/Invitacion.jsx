2788108f0cb46c805d360c25013e87cd96622ed7
Tomas Reveron <tomyreveroncito@gmail.com>
2026-02-07 15:13:32 -0400

    feat: habilitar botón de enlace a transmisión en vivo en el componente Invitacion

--- src/pages/Invitacion.jsx
+++ src/pages/Invitacion.jsx
@@ -318,9 +318,8 @@ export default function Invitacion() {
                       <>
                         <button
                           className="action-btn"
-                          disabled
+                          onClick={() => window.open('https://youtube.com/live/TtFkkrR_W1g', '_blank')}
                           title="En vivo"
-                          style={{ opacity: 0.8, cursor: 'default' }}
                         >
                           {/* Camera Icon */}
                           <svg width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
